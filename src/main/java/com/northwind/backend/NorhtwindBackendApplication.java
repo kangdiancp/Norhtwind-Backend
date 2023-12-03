79e9771a7c49e57e097ff610cf809cf126ec14a5
kangdiancp <kangdiancp@gmail.com>
2023-12-04 00:06:51 +0700

    init project

---
+++ src/main/java/com/northwind/backend/NorhtwindBackendApplication.java
@@ -0,0 +1,13 @@
+package com.northwind.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorhtwindBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NorhtwindBackendApplication.class, args);
+	}
+
+}
